3156b2482e923b92062477894c3a158934fac6af
Playwright Simple <playwright-simple@softhill.local>
2025-11-14 18:31:17 -0300

    Fix import error: move element_interactions.py into package directory

--- playwright_simple/core/playwright_commands/element_interactions/__init__.py
+++ playwright_simple/core/playwright_commands/element_interactions/__init__.py
@@ -7,14 +7,10 @@ Refactored from element_interactions.py to improve maintainability.
 """
 
 from .element_interactions import ElementInteractions
-from .click_handler import ClickHandler
-from .type_handler import TypeHandler
 from .focus_helper import FocusHelper
 
 __all__ = [
     'ElementInteractions',
-    'ClickHandler',
-    'TypeHandler',
     'FocusHelper',
 ]
 
